2bae42f4e06219a471a21de578c71c57b6fb2c79
LoisGarcion <lois.garcion34@gmail.com>
2024-03-14 17:21:52 +0100

    Le script marche, on donne une date de début et il crée de la donnée dans 2 salle a 30 minutes d'intervalles entre les données

--- ScriptGenerationDonnees.js
+++ ScriptGenerationDonnees.js
@@ -8,10 +8,6 @@ const pool = new Pool({
     ssl: true
 })
 
-const express = require('express')
-const app = express()
-const PORT = 8080
-
 /*
 pool.query('CALL ADD_PASSAGE($1, $2)', [1, 2], (error, results, fields) => {
     if (error) {
@@ -22,9 +18,9 @@ pool.query('CALL ADD_PASSAGE($1, $2)', [1, 2], (error, results, fields) => {
 });
 */
 
-pool.query(
+/*pool.query(
     'CALL add_passage_date($1, $2, $3::timestamp with time zone)',
-    [1, null, '2024-03-14 10:00:00+01'],
+    [1, null, '2024-03-14 18:20:00+01'],
     (error, results, fields) => {
         if (error) {
             console.error('Error calling stored procedure: ' + error);
@@ -33,4 +29,59 @@ pool.query(
         console.log(results);
     }
 );
+*/
 
+function callProcedure(param1, param2, param3){
+    if(param3 === null){
+        return;
+    }
+    pool.query(
+        'CALL add_passage_date($1, $2, $3::timestamp with time zone)',
+        [param1, param2, param3],
+        (error, results, fields) => {
+            if (error) {
+                console.error('Error calling stored procedure: ' + error);
+                return;
+            }
+        }
+    );
+}
+
+// Function to generate fake data
+function generateFakeData() {
+    let date = new Date('2024-03-20T18:00:00+01:00');
+
+    for (let i = 0; i < 1000; i++) {
+        let entryRoom = Math.random() < 0.5 ? 1 : 2;
+        let enter = Math.random() < 0.46;
+        let both = Math.random() < 0.2;
+        if(both){
+            callProcedure(entryRoom,entryRoom === 1 ? 2 : 1, date);
+            date = new Date(date.getTime() + 1000*60*30);
+            console.log("Insert entrant in : " + entryRoom + " and out in " + (entryRoom === 1 ? 2 : 1) + " at " + date);
+        }
+        if(entryRoom === 1) {
+            if(enter) {
+                callProcedure(1, null, date);
+                date = new Date(date.getTime() + 1000*60*30);
+            }
+            else {
+                callProcedure(null, 1, date);
+                date = new Date(date.getTime() + 1000*60*30);
+            }
+        }
+        if(entryRoom === 2) {
+            if(enter) {
+                callProcedure(2, null, date);
+                date = new Date(date.getTime() + 1000*60*30);
+            }
+            else {
+                callProcedure(null, 2, date);
+                date = new Date(date.getTime() + 1000*60*30);
+            }
+        }
+    }
+    console.log("Data generated.");
+}
+
+generateFakeData();
