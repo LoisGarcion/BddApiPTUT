f8a64b07d181a4a1edcea6054aba45c774b66326
LoisGarcion <lois.garcion34@gmail.com>
2024-03-26 20:04:03 +0100

    modif du script

--- ScriptGenerationDonnees.js
+++ ScriptGenerationDonnees.js
@@ -31,43 +31,68 @@ pool.query('CALL ADD_PASSAGE($1, $2)', [1, 2], (error, results, fields) => {
 );
 */
 
-function callProcedure(param1, param2, param3){
-    if(param3 === null){
-        return;
-    }
-    pool.query(
-        'CALL add_passage_date($1, $2, $3::timestamp with time zone)',
-        [param1, param2, param3],
-        (error, results, fields) => {
-            if (error) {
-                console.error('Error calling stored procedure: ' + error);
-                return;
-            }
+async function callProcedure(param1, param2, param3){
+    return new Promise((resolve, reject) => {
+        if (param3 === null) {
+            resolve();
         }
-    );
+        console.log("Je génère");
+        pool.query(
+            'CALL add_passage_date($1, $2, $3::timestamp with time zone)',
+            [param1, param2, param3],
+            (error, results, fields) => {
+                if (error) {
+                    //console.error('Error calling stored procedure: ' + error);
+                    resolve('Error calling stored procedure: ' + error);
+                }
+                resolve();
+            }
+        );
+    });
+}
+
+async function getNumberOfPeopleLastPassage(numsalle){
+    return new Promise((resolve, reject
+    ) => {
+        pool.query(
+            'SELECT nbpersonne FROM passage WHERE numerosalle = $1 ORDER BY datepassage DESC LIMIT 1',[numsalle],
+            (error, results) => {
+                if (error) {
+                    console.error('Error calling stored procedure: ' + error);
+                    resolve('Error calling stored procedure: ' + error);
+                }
+                console.log(results.rows[0].nbpersonne);
+                resolve(results.rows[0].nbpersonne);
+            }
+        );
+    });
 }
 
 // Function to generate fake data
-function generateFakeData() {
-    let date = new Date('2024-03-15T08:00:00+01:00');
+async function generateFakeData() {
+    let date = new Date('2024-01-01T08:10:00+01:00');
 
-    for (let i = 0; i < 2000; i++) {
-        // random number between 8 and 14
-        let entryRoom = Math.floor(Math.random() * (14 - 8 + 1)) + 8;
+    for (let i = 0; i < 30000; i++) {
+        // random number between 5 and 15 included
+        let entryRoom = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
         let enter = Math.random() < 0.46;
-        if(enter) {
-            callProcedure(entryRoom, null, date);
-            date = new Date(date.getTime() + 1000*60*30);
+        if (enter) {
+            await callProcedure(entryRoom, null, date);
+            date = new Date(date.getTime() + 1000 * 60 * 5);
+        } else {
+           await callProcedure(null, entryRoom, date);
+            date = new Date(date.getTime() + 1000 * 60 * 5);
         }
-        else {
-            callProcedure(null, entryRoom, date);
-            date = new Date(date.getTime() + 1000*60*30);
-        }
-        if(date.getHours()>= 20){
-            date = new Date(date.getTime() + 1000*60*60*12)
+        if (date.getHours() >= 19) {
+            for (let j = 5; j <= 15; j++) {
+                let nb = await getNumberOfPeopleLastPassage(j);
+                for (let k = 0; k < nb; k++) {
+                    await callProcedure(null, j, date);
+                    date = new Date(date.getTime() + 1000 * 60);
+                }
+            }
+            date = new Date(date.getTime() + 1000 * 60 * 60 * 12)
         }
     }
-    console.log("Data generated.");
 }
-
-generateFakeData();
+generateFakeData().then(console.log("fini"));
